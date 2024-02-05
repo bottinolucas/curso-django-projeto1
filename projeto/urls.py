03230accbad03d63d6b0d28028f575a7ddc45dd8
bottinolucas <lucasgabottino@gmail.com>
2024-02-05 17:18:38 -0300

    learn: aprendendo a usar as urls no django e django views

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+#HTTP REQUEST <- HTTP RESPONSE
+
+#HTTP REQUEST
+def my_view(request):
+    return HttpResponse("UMA STRING!")
+    #RETURN HTTP RESPONSE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
